4b66e976075d1a4b82de3881a7c0a2f86d0f5019
hubertus.putu1 <hubertus.putu1@gmail.com>
2019-10-10 21:42:14 +0700

    refactor appbar

---
+++ src/containers/appBar/appBar.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withStyles } from '@material-ui/core/styles';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  MenuItem,
+  Menu,
+  Button,
+} from '@material-ui/core';
+import { MoreVert } from '@material-ui/icons';
+
+import { mapDispatch, mapState } from './appBar.controller';
+
+const styles = theme => ({
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    // display: 'none',
+    // [theme.breakpoints.up('sm')]: {
+    //   display: 'block',
+    // },
+  },
+});
+
+class AppBarComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false,
+      anchorEl: null,
+    };
+  }
+
+  handleClick = event => {
+    this.setState({ open: true, anchorEl: event.currentTarget });
+  };
+
+  handleClose = url => {
+    this.setState({ open: false, anchorEl: null }, () => {
+      if (url) this.props.history.push(url);
+    });
+  };
+
+  renderTopAppBar = classes => {
+    return (
+      <AppBar>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+            onClick={e => this.handleClick(e)}
+          >
+            <MoreVert />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            BPR Dalung
+          </Typography>
+          <Button color="inherit">Import</Button>
+          <Button color="inherit">Export</Button>
+        </Toolbar>
+      </AppBar>
+    );
+  };
+
+  renderNavOptions = () => {
+    const { open, anchorEl } = this.state;
+    return (
+      <Menu
+        id="long-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={open}
+        PaperProps={{
+          style: {
+            maxHeight: 48 * 4.5,
+            width: 200,
+          },
+        }}
+        onClose={() => this.handleClose()}
+      >
+        <MenuItem selected onClick={() => this.handleClose('/')}>
+          Home
+        </MenuItem>
+      </Menu>
+    );
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        {this.renderTopAppBar(classes)}
+        {this.renderNavOptions()}
+      </div>
+    );
+  }
+}
+
+export default connect(
+  mapState,
+  mapDispatch
+)(withStyles(styles)(AppBarComponent));
--- src/containers/home/home.js
+++ src/containers/home/home.js
@@ -19,6 +19,8 @@ import { MoreVert, NavigateBefore, NavigateNext } from '@material-ui/icons';
 
 import { mapDispatch, mapState } from './home.controller';
 
+import AppBarComponent from '../appBar';
+
 const styles = theme => ({
   root: {
     flexGrow: 1,
@@ -29,36 +31,12 @@ const styles = theme => ({
     width: 50,
     color: 'primary',
   },
-  bottomAppBar: {
-    top: 'auto',
-    bottom: 0,
-    display: 'flex',
-  },
-  bottomToolbar: {
-    display: 'flex',
-    justifyContent: 'space-between',
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-    // display: 'none',
-    // [theme.breakpoints.up('sm')]: {
-    //   display: 'block',
-    // },
-  },
   text: {
     padding: theme.spacing(2, 2, 0),
   },
   paper: {
     paddingTop: 50,
     paddingBottom: 50,
-  },
-  optionsPage: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    color: 'white',
   },
 });
 
@@ -73,73 +51,17 @@ class Home extends Component {
     };
   }
 
-  handleClick = event => {
-    this.setState({ open: true, anchorEl: event.currentTarget });
-  };
-
-  handleClose = url => {
-    this.setState({ open: false, anchorEl: null }, () => {
-      if (url) this.props.history.push(url);
-    });
-  };
-
   handleChange = name => event => {
     this.setState({ ...this.state, [name]: event.target.value });
   };
 
   componentDidMount = () => {};
 
-  renderTopAppBar = classes => {
-    return (
-      <AppBar>
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-            onClick={e => this.handleClick(e)}
-          >
-            <MoreVert />
-          </IconButton>
-          <Typography className={classes.title} variant="h6" noWrap>
-            BPR
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    );
-  };
-
-  renderNavOptions = () => {
-    const { open, anchorEl } = this.state;
-    return (
-      <Menu
-        id="long-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={open}
-        PaperProps={{
-          style: {
-            maxHeight: 48 * 4.5,
-            width: 200,
-          },
-        }}
-        onClose={() => this.handleClose()}
-      >
-        <MenuItem selected onClick={() => this.handleClose('/')}>
-          Home
-        </MenuItem>
-        <MenuItem onClick={() => this.handleClose('/')}>My Pokemon</MenuItem>
-      </Menu>
-    );
-  };
-
   render() {
     const { classes } = this.props;
     return (
       <div className={classes.root}>
-        {this.renderTopAppBar(classes)}
-        {this.renderNavOptions()}
+        <AppBarComponent />
         <Paper square className={classes.paper}></Paper>
       </div>
     );
